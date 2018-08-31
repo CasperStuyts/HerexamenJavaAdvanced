6e4cf7819a0fd4a5f5e4652d32978f54aa1828fe
CasperStuyts <33087689+CasperStuyts@users.noreply.github.com>
2018-08-31 14:11:43 +0200

    Vraag 2 + screenshot document
    Examen vraag 2 volledig opgelost en een document met de screenshots van de console aangemaakt.

--- SPRING-EXAMEN-JAVA2-VRAAG2/src/main/java/edu/ap/spring/Exam.java
+++ SPRING-EXAMEN-JAVA2-VRAAG2/src/main/java/edu/ap/spring/Exam.java
@@ -15,28 +15,34 @@ public class Exam {
 	// Maak gebruik van lambdas en streams om een array met alle
 	// getallen terug te geven die deelbaar zijn door twee
 	// 2 punten
+	
 	public int[] getTimesTwo(int[] numbers) {
-		return numbers;
+		return  IntStream.of(numbers).filter(x -> x % 2==0).toArray();
 	}
 	
 	// Maak gebruik van lambdas en streams om alle uppercase characters
 	// te tellen in de gegeven string
 	// 1 punt
 	public int countUppercaseCharacters(String sentence) {
-		return 0;
+		return (int) sentence.chars().filter(Character::isUpperCase).count();
 	}
 	
 	// Maak gebruik van lambdas en streams om het aantal keren te tellen
 	// dat string s voorkomt (zowel uppercase als lowercase) in de lijst names
 	// 1 punt
 	public int countChar(List<String> names, String s) {
-		return 0;
+		String lowercasestring = names.stream().map(Object::toString).collect(Collectors.joining(",")).toLowerCase();
+		return (int)((CharSequence) lowercasestring).chars().filter(ch -> ch == 'e').count();
 	}
-	
 	// Maak gebruik van lambdas en streams om het hoogste Y coordinaat
 	// te vinden
 	// 2 punten
 	public int getMaxY(List<Point> points) {
-		return 0;
+		Double max = points
+			      .stream()
+			      .mapToDouble(v -> v.getY())
+			      .max().orElseThrow(NoSuchElementException::new);
+		int test = max.intValue();
+		return test;
 	}	
 }
